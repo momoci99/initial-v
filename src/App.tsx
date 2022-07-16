183777edc63fe641f768ca97271158c108791574
YeongKwonMo <momoci99@gmail.com>
2022-07-16 17:16:24 +0900

    fix:reset css 적용 순서 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./assets/styles/App.css";
-import "./assets/styles/reset.css";
+
 import Header from "./Header";
 import Hero from "./Hero";
 import SubHero from "./SubHero";
